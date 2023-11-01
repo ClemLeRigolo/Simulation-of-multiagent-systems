159bb5550da2f9388ef89a9b8ab7b62cb8430969
loyerg <gavril.loyer@grenoble-inp.org>
2023-11-01 10:33:22 +0100

    Correction de SchellingGame

--- src/SchellingGame.java
+++ src/SchellingGame.java
@@ -12,145 +12,155 @@ public class SchellingGame {
 }
 
 class SchellingGameEngine extends CellGameEngine {
-        private int threshold;
-        private int colorNumber;
+    private int threshold;
+    private int colorNumber;
 
-        //Array of Color
-        private Color[] colors;
-        HashSet<Cell> coloredCells = new HashSet<>();
-        HashSet<Cell> emptyCells = new HashSet<>();
+    // Array of Color
+    private Color[] colors;
+    HashSet<Cell> coloredCells = new HashSet<>();
+    HashSet<Cell> emptyCells = new HashSet<>();
 
-        public SchellingGameEngine(GUISimulator gui, int gridSize, int cellNumber, int threshold, int colorNumber) {
-            super(gui, gridSize, cellNumber, colorNumber);
-            this.threshold = threshold;
-            this.colorNumber = colorNumber;
-            this.colors = new Color[colorNumber];
+    public SchellingGameEngine(GUISimulator gui, int gridSize, int cellNumber, int threshold, int colorNumber) {
+        super(gui, gridSize, cellNumber, colorNumber);
+        this.threshold = threshold;
+        this.colorNumber = colorNumber;
+        this.colors = new Color[colorNumber];
 
-            //Generate random and different colors
-            for(int i = 0; i < colorNumber; i++){
-                colors[i] = new Color((int) (Math.random() * 0x1000000));
-                for(int j = 0; j < i; j++){
-                    if(colors[i] == colors[j]){
-                        i--;
-                    }
-                }
-            }
-            for(int i = 0; i < gridSize; i++) {
-                for(int j = 0; j < gridSize; j++){
-                    emptyCells.add(grid.getGrid()[i][j]);
-                }
-            }
-            firstGeneration(cellNumber);
-            draw();
-        }
-
-        @Override
-        protected void init(){
-            //All cells are in emptyCells
-
-        }
-
-        @Override
-        protected void draw(){
-            gui.reset();
-            for (int i = 0; i < grid.getSize(); i++) {
-                for (int j = 0; j < grid.getSize(); j++) {
-                    //If the cell is in emptyCell
-                    if(emptyCells.contains(grid.getCell(i,j))) {
-                        gui.addGraphicalElement(new Rectangle(i * this.cellWidth + 60, j * this.cellWidth + 60, Color.LIGHT_GRAY, Color.WHITE, this.cellWidth));
-                    }else{
-                        //System.out.println("color of cell " + i + " " + j + " is " + colors[grid.getCell(i,j).getCurrentState()]+" and state is " + grid.getCell(i,j).getCurrentState());
-                        gui.addGraphicalElement(new Rectangle(i * this.cellWidth + 60, j * this.cellWidth+60, colors[grid.getCell(i,j).getCurrentState()], colors[grid.getCell(i,j).getCurrentState()], this.cellWidth));
-                    }
+        // Generate random and different colors
+        for (int i = 0; i < colorNumber; i++) {
+            colors[i] = new Color((int) (Math.random() * 0x1000000));
+            for (int j = 0; j < i; j++) {
+                if (colors[i] == colors[j]) {
+                    i--;
                 }
             }
         }
-
-        @Override
-        public void restart(){
-            grid = new Grid(this.gridSize, this.stateNumber);
-            for(int i = 0; i < colorNumber; i++){
-                colors[i] = new Color((int) (Math.random() * 0x1000000));
-                for(int j = 0; j < i; j++){
-                    if(colors[i] == colors[j]){
-                        i--;
-                    }
-                }
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                emptyCells.add(grid.getGrid()[i][j]);
             }
-            for(int i = 0; i < gridSize; i++) {
-                for(int j = 0; j < gridSize; j++){
-                    emptyCells.add(grid.getGrid()[i][j]);
-                }
-            }
-            firstGeneration(this.cellNumber);
-            draw();
         }
+        firstGeneration(cellNumber);
+        draw();
+    }
 
-        public void calculateNeighbours(){
-            //Neighbours of coloredCells are the neighbours cells with a different state different from 0
+    @Override
+    protected void init() {
+        // All cells are in emptyCells
 
-            for(Cell cell : coloredCells){
-                int nbNeighbours = 0;
-                for(int k = -1; k <= 1; k++) {
-                    for(int l = -1; l <= 1; l++){
-                        if(!(k == 0 && l == 0) && cell.getX() + k >= 0 && cell.getX() + k < gridSize && cell.getY() + l >= 0 && cell.getY() + l < gridSize){
-                            if(grid.getGrid()[cell.getX() + k][cell.getY() + l].getPreviousState() != 0 && grid.getGrid()[cell.getX() + k][cell.getY() + l].getPreviousState() != cell.getPreviousState()){
-                                nbNeighbours++;
-                            }
+    }
+
+    @Override
+    protected void draw() {
+        gui.reset();
+        for (int i = 0; i < grid.getSize(); i++) {
+            for (int j = 0; j < grid.getSize(); j++) {
+                // If the cell is in emptyCell
+                if (emptyCells.contains(grid.getCell(i, j))) {
+                    gui.addGraphicalElement(new Rectangle(i * this.cellWidth + 60, j * this.cellWidth + 60,
+                            Color.LIGHT_GRAY, Color.WHITE, this.cellWidth));
+                } else {
+                    // System.out.println("color of cell " + i + " " + j + " is " +
+                    // colors[grid.getCell(i,j).getCurrentState()]+" and state is " +
+                    // grid.getCell(i,j).getCurrentState());
+                    gui.addGraphicalElement(new Rectangle(i * this.cellWidth + 60, j * this.cellWidth + 60,
+                            colors[grid.getCell(i, j).getCurrentState()], colors[grid.getCell(i, j).getCurrentState()],
+                            this.cellWidth));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void restart() {
+        grid = new Grid(this.gridSize, this.stateNumber);
+        for (int i = 0; i < colorNumber; i++) {
+            colors[i] = new Color((int) (Math.random() * 0x1000000));
+            for (int j = 0; j < i; j++) {
+                if (colors[i] == colors[j]) {
+                    i--;
+                }
+            }
+        }
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                emptyCells.add(grid.getGrid()[i][j]);
+            }
+        }
+        firstGeneration(this.cellNumber);
+        draw();
+    }
+
+    public void calculateNeighbours() {
+        // Neighbours of coloredCells are the neighbours cells with a different state
+        // different from 0
+
+        for (Cell cell : coloredCells) {
+            int nbNeighbours = 0;
+            for (int k = -1; k <= 1; k++) {
+                for (int l = -1; l <= 1; l++) {
+                    if (!(k == 0 && l == 0) && cell.getX() + k >= 0 && cell.getX() + k < gridSize
+                            && cell.getY() + l >= 0 && cell.getY() + l < gridSize) {
+                        if (grid.getGrid()[cell.getX() + k][cell.getY() + l].getCurrentState() != 0
+                                && grid.getGrid()[cell.getX() + k][cell.getY() + l].getCurrentState() != cell
+                                        .getCurrentState()) {
+                            nbNeighbours++;
                         }
                     }
                 }
-                cell.setNbNeighbours(nbNeighbours);
-                //System.out.println("Cell at " + cell.getX() + " " + cell.getY() + " has " + nbNeighbours + " neighbours");
             }
+            cell.setNbNeighbours(nbNeighbours);
+            // System.out.println("Cell at " + cell.getX() + " " + cell.getY() + " has " +
+            // nbNeighbours + " neighbours");
         }
+    }
 
-        public void firstGeneration(int cellNumber){
-            // set random cells to random states
-            System.out.println(cellNumber);
-            for(int i = 0; i < cellNumber; i++) {
-                int x = (int) (Math.random() * gridSize);
-                int y = (int) (Math.random() * gridSize);
-                int state = (int) (Math.random() * colorNumber);
-                grid.getGrid()[x][y].setPreviousState(state);
-                grid.getGrid()[x][y].setCurrentState(grid.getGrid()[x][y].getPreviousState());
-                coloredCells.add(grid.getGrid()[x][y]);
-                emptyCells.remove(grid.getGrid()[x][y]);
-            }
-            calculateNeighbours();
+    public void firstGeneration(int cellNumber) {
+        // set random cells to random states
+        System.out.println(cellNumber);
+        for (int i = 0; i < cellNumber; i++) {
+            int x = (int) (Math.random() * gridSize);
+            int y = (int) (Math.random() * gridSize);
+            int state = (int) (Math.random() * colorNumber);
+            grid.getGrid()[x][y].setPreviousState(state);
+            grid.getGrid()[x][y].setCurrentState(grid.getGrid()[x][y].getPreviousState());
+            coloredCells.add(grid.getGrid()[x][y]);
+            emptyCells.remove(grid.getGrid()[x][y]);
         }
+        calculateNeighbours();
+    }
 
     @Override
     protected void nextGeneration() {
-        //If a cell has more than threshold neighbours with a different state, it becomes an empty cell and moves to a random empty cell
-        //Empty cells are those with state 0
+        // If a cell has more than threshold neighbours with a different state, it
+        // becomes an empty cell and moves to a random empty cell
+        // Empty cells are those with state 0
 
         HashSet<Cell> movingCells = new HashSet<>();
 
-        for(Cell cell : coloredCells){
-            if(cell.getNbNeighbours() >= threshold){
-                emptyCells.add(cell);
+        for (Cell cell : coloredCells) {
+            if (cell.getNbNeighbours() >= threshold) {
                 movingCells.add(cell);
             }
         }
 
-        for(Cell cell : movingCells){
-            //Move the cell to a random empty cell
+        for (Cell cell : movingCells) {
+            // Move the cell to a random empty cell
             int x = (int) (Math.random() * gridSize);
             int y = (int) (Math.random() * gridSize);
-            while(grid.getGrid()[x][y].getCurrentState() != 0){
+            while (grid.getGrid()[x][y].getCurrentState() != 0) {
                 x = (int) (Math.random() * gridSize);
                 y = (int) (Math.random() * gridSize);
             }
-            //grid.getGrid()[x][y].setPreviousState(cell.getPreviousState());
-            grid.getGrid()[x][y].setCurrentState(cell.getPreviousState());
+            // grid.getGrid()[x][y].setPreviousState(cell.getPreviousState());
+            grid.getGrid()[x][y].setCurrentState(cell.getCurrentState());
+            cell.setCurrentState(0);
             coloredCells.remove(cell);
             emptyCells.remove(grid.getGrid()[x][y]);
             coloredCells.add(grid.getGrid()[x][y]);
             emptyCells.add(cell);
         }
         calculateNeighbours();
-
 
     }
 }
