88f7b3f4f086fe94030519b410f7b00c4382f107
Gavril Loyer <Gavril.Loyer@grenoble.org>
2023-11-16 19:17:35 +0100

    Ajout de Comparable dans Event

--- src/Event.java
+++ src/Event.java
@@ -2,7 +2,7 @@
  * Un événement ?
  * Classe abstraite définissant les méthodes que tout événement devrait avoir: une date et quelque chose à effectuer.
  */
-public abstract class Event {
+public abstract class Event implements Comparable<Event>{
     private long date;
 
     public Event(long date) {
@@ -19,4 +19,14 @@ public abstract class Event {
 
     abstract void execute();
 
+    public int compareTo(Event event) {
+        if (event.getDate() == this.getDate()) {
+            return 0;
+        }
+        if (event.getDate() > this.getDate()) {
+            return -1;
+        }
+
+        return 1;
+    }
 }
